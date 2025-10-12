2905f5715f8c1dca89f090ca5e0fb3f0551e2829
markbrown88 <markbrown8@gmail.com>
2025-10-12 09:50:27 -0400

    Exclude utility scripts from repository and build process

--- add-game-status.sql
+++
@@ -1,5 +0,0 @@
--- Add status column to Game table
-ALTER TABLE "Game" ADD COLUMN IF NOT EXISTS status TEXT DEFAULT 'not_started';
-
--- Update any existing null values to 'not_started'
-UPDATE "Game" SET status = 'not_started' WHERE status IS NULL;
